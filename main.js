0502546dae6b9924a523158379e2b26ee16872d9
yychiaki <e14008@std.it-college.ac.jp>
2014-11-17 22:05:03 +0900

    141117

--- main.js
+++ main.js
@@ -5,16 +5,77 @@ var game;
 var gs = {
 	height:320
 	,width:320
-	,fps:15
+	,fps:30
 };
+var Pit = Class.create(Sprite,{
+	initialize:function(x,y){
+		enchant.Sprite.call(this,48,48);
+		this.image = game.assets['./assets/images/mogura.png'];
+		this.x = x;
+		this.y = y;
+		this.mode = 2;
+		this.nextMode = 0;
+		this.waitFor = game.frame + rand(100);
+		console.log(game.frame +"|"+ this.waitFor);
+
+	},
+	move:function(){
+		switch(this.mode) {
+			case 0:
+			this.frame++;
+			if(this.frame > 4 - 1){
+				this.changeMode(1,100);
+			}
+			break;
+			case 1:
+			this.frame--;
+			if (this.frame < 1){
+				this.changeMode(0,100);
+			}
+			break;
+			case 2:
+			if (game.frame > this.waitFor) {
+				this.mode = this.nextMode;
+			}
+		}
+	},
+	changeMode:function(nextmode,random){
+		this.mode = 2;
+		this.nextMode = nextmode;
+		this.waitFor = game.frame + rand(random);
+	},
+	onenterframe:function(){
+		if(game.frame % 2 != 0){
+			this.move();
+		}
+	},
+	ontouchstart:function(){
+		this.hit();
+	},
+	hit:function(){
+		if(this.frame ===5 ){
+			return ;
+		}
+		if(this.frame > 2-1){
+			this.frame = 5;
+			this.changeMode(1,10);
+		}
+	}
+});
 
 window.onload = function(){
 	game = new Core(gs.width,gs.height);
 	game.fps = gs.fps;
-	
+	game.preload('./assets/images/mogura.png');
 	game.onload = function(){
+		var pit = new Pit(100,100);
+		game.rootScene.addChild(pit);
+
 
 	};
-	
 	game.start();
 };
+
+function rand(num) {
+	return ~~(Math.random() * num);
+}
